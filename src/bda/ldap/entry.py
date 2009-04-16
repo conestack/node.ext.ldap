3469d740357027fe016e3e58dae2f286eb429c9d
rnix <rnix@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-04-16 15:21:23 +0000

    reading done, adding work in progress
    git-svn-id: https://svn.plone.org/svn/collective/bda.ldap/trunk@84360 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- src/bda/ldap/entry.py
+++ src/bda/ldap/entry.py
@@ -1,9 +1,19 @@
 # Copyright 2008-2009, BlueDynamics Alliance, Austria - http://bluedynamics.com
 # GNU General Public Licence Version 2 or later
 
+import types
 from zodict.node import Node
 from bda.ldap import ONELEVEL
 from bda.ldap import LDAPSession
+
+from ldap import MOD_ADD
+from ldap import MOD_DELETE
+from ldap import MOD_REPLACE
+
+ACTION_ADD = 0
+ACTION_MODIFY = 1
+ACTION_DELETE = 2
+ACTION_READ = 3
 
 class LDAPEntry(Node):
     """An LDAP Entry.
@@ -21,6 +31,10 @@ class LDAPEntry(Node):
             self._session = LDAPSession(props)
             self._session.setBaseDN(self.DN)
         self.attributes = dict()
+        self._orginattrs = dict()
+        self._changed = False
+        self._action = ACTION_ADD
+        self._keys = list()
             
     @property
     def DN(self):
@@ -40,13 +54,17 @@ class LDAPEntry(Node):
         if len(entry) != 1:
             raise RuntimeError(u"Fatal. Expected entry does not exist or "
                                 "more than one entry found")
-        self.attributes.update(entry[0][1])
+        attrs = entry[0][1]
+        for key, item in attrs.items():
+            if len(item) == 1:
+                attrs[key] = item[0]
+        self.attributes.update(attrs)
+        self._orginattrs.update(attrs)
     
     def __iter__(self):
-        if not hasattr(self, '_keys'):
-            self._keys = list()
+        if not self._keys:
             children = self._session.search('(objectClass=*)', ONELEVEL,
-                                            attrsonly=1)
+                                            baseDN=self.DN, attrsonly=1)
             for dn, attrs in children:
                 self._keys.append(dn[:dn.rfind(self.DN)].strip(','))
         for key in self._keys:
@@ -61,12 +79,88 @@ class LDAPEntry(Node):
             child = Node.__getitem__(self, key)
         except KeyError, e:
             child = LDAPEntry()
+            child._action = ACTION_READ
             self[key] = child
         return child
     
     def __setitem__(self, key, val):
-        self._keys.remove(key)
+        try:
+            self._keys.remove(key)
+        except ValueError, e: pass
         val._session = self._session
+        if key in self.keys():
+            val._changed = True
+            val._action = ACTION_MODIFY
         Node.__setitem__(self, key, val)
         self._keys.append(key)
-        val.readattrs()
+        if val._action == ACTION_READ:
+            val.readattrs()
+            val._action = None
+        else:
+            val._changed = True
+            val._action = ACTION_ADD
+        self._changed = True
+    
+    def __delitem__(self, key):
+        """Do not delete immediately. Just mark Entry to be deleted and
+        remove key from self._keys.
+        """
+        todelete = self[key]
+        todelete._changed = True
+        todelete._action = ACTION_DELETE
+        self._keys.remove(key)
+    
+    def __call__(self):
+        for entry in Node.values(self):
+            entry()
+        self._checkattrchanged()
+        if not self._changed:
+            return
+        action = self._action
+        if action == ACTION_ADD:
+            self._add()
+        if action == ACTION_MODIFY:
+            self._modify()
+        if action == ACTION_DELETE:
+            self._delete()
+    
+    def _checkattrchanged(self):
+        if len(self.attributes) != len(self._orginattrs):
+            self._changed = True
+            return
+        for key in self.attributes.keys():
+            if self.attributes[key] != self._orginattrs[key]:
+                self._changed = True
+                return
+    
+    def _add(self):
+        print 'add'
+        self._session.add(self.DN, self.attributes)
+    
+    def _modify(self):
+        print 'modify'
+        modlist = list()
+        orginkeys = self._orginattrs.keys()
+        attrkeys = self.attributes.keys()
+        for key in orginkeys:
+            # MOD_DELETE
+            if not key in attrkeys:
+                moddef = (MOD_DELETE, key, None)
+                modlist.append(moddef)
+        for key in attrkeys:
+            # MOD_ADD
+            if not key in orginkeys:
+                moddef = (MOD_ADD, key, self.attributes[key])
+                modlist.append(moddef)
+            # MOD_REPLACE
+            if key in orginkeys:
+                if self.attributes[key] != self._orginattrs[key]:
+                    moddef = (MOD_REPLACE, key, self.attributes[key])
+                    modlist.append(moddef)
+        if modlist:
+            self._session.modify(self.DN, modlist)
+    
+    def _delete(self):
+        print 'delete'
+        self.session.delete(self.DN)
+        Node.__delitem__(self.__parent__, self.__name__)
