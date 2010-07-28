b51448f0d8656aefec12757c2c435372be5224d8
chaoflow <chaoflow@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-07-28 19:41:38 +0000

    LDAPProps extended for reconnect and ssl/tls
    git-svn-id: https://svn.plone.org/svn/collective/bda.ldap/trunk@122206 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- src/bda/ldap/properties.py
+++ src/bda/ldap/properties.py
@@ -6,27 +6,66 @@ class LDAPServerProperties(object):
     """
     
     def __init__(self,
-                 server='localhost',
-                 port=389,
+                 server=None,
+                 port=None,
                  user='',
                  password='',
                  cache=True,
-                 timeout=43200):
+                 timeout=43200,
+                 uri=None,
+                 #start_tls=1,
+                 #tls_cacertfile=None,
+                 #tls_cacertdir=None,
+                 #tls_clcertfile=None,
+                 #tls_clkeyfile=None,
+                 retry_max=1,
+                 retry_delay=10.0,
+                 ):
         """Take the connection properties as arguments.
         
-        @param server: servername, defaults to 'localhost'
-        @param port: server port, defaults to 389
+	 SSL/TLS still unsupported
+
+        @param server: DEPRECATED use uri! servername, defaults to 'localhost'
+        @param port: DEPRECATED uss uri! server port, defaults to 389
         @param user: username to bind, defaults to ''
         @param password: password to bind, defaults to ''
         @param cache: Bool wether to enable caching or not, defaults to True
         @param timeout: Cache timeout in seconds. only takes affect if cache
                         is enabled.
+        @param uri: overrides server/port, forget about server and port, use
+        this to specify how to access the ldap server, eg:
+            - ldapi:///path/to/socket
+            - ldap://<server>:<port> (will try start_tls, which you can
+              enforce, see start_tls)
+            - ldaps://<server>:<port>
+        @param start_tls: Determines if StartTLS extended operation is tried on
+        a LDAPv3 server, iff the LDAP URL scheme is ldap:. If LDAP URL scheme
+        is not ldap: (e.g. ldaps: or ldapi:) this parameter is ignored.
+            0       Don't use StartTLS ext op
+            1       Try StartTLS ext op but proceed when unavailable
+            2       Try StartTLS ext op and re-raise exception if it fails
+        @param tls_cacertfile:
+        @param tls_cacertdir:
+        @param tls_clcertfile:
+        @param tls_clkeyfile:
+        @param retry_max: Maximum count of reconnect trials
+        @param retry_delay: Time span to wait between two reconnect trials
         """
-        self.server = server
-        self.port = port
+        if uri is None:
+            # old school
+            self.server = server or 'localhost'
+            self.port = port or 389
+        self.uri = uri
         self.user = user
         self.password = password
         self.cache = cache
         self.timeout = timeout
+        #self.start_tls = start_tls
+        #self.tls_cacertfile = tls_cacertfile
+        #self.tls_cacertdir = tls_cacertdir
+        #self.tls_clcertfile = tls_clcertfile
+        #self.tls_clkeyfile = tls_clkeyfile
+        self.retry_max = retry_max
+        self.retry_delay = retry_delay
 
 LDAPProps = LDAPServerProperties
