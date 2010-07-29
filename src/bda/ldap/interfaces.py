d66c07616dd0f61275e53a6d4e2be6c6c841866f
ielectric <ielectric@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-07-29 17:02:06 +0000

    add incomplete groups interface
    git-svn-id: https://svn.plone.org/svn/collective/bda.ldap/trunk@122322 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- src/bda/ldap/interfaces.py
+++ src/bda/ldap/interfaces.py
@@ -2,13 +2,23 @@
 # GNU General Public Licence Version 2 or later
 
 from zope.interface import Interface
+from zope.schema import TextLine, Choice
+
 
 class ICacheProviderFactory(Interface):
     """Create some ICacheProvider implementing object on __call__.
-    
+
     Must be registered as utility.
     """
-    
+
     def __call__():
-        """See above.
-        """
+        """See above.  """
+
+
+class ILDAPGroupsConfig(Interface):
+    props = Object(schema=IProps)
+    baseDN = TextLine(title="Base DN", required=True)
+    id_attr = TextLine(title="ID attribute name", required=True)
+    login_attr = TextLine(title="Login attribute name", required=True)
+    scope = Choice(title="Scope for searching", vocabulary=)
+    queryFilter = TextLine(title="Query filter")
