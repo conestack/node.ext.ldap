1459f275dd010322b3ddc6cfa137b71d58680db1
chaoflow <chaoflow@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-07-30 18:24:20 +0000

    groups will stick to the node API - tests succeed again
    users will be virtual childs of groups, implementation in group will be
    minimal.
    
    git-svn-id: https://svn.plone.org/svn/collective/bda.ldap/trunk@122438 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- src/bda/ldap/groups.py
+++ src/bda/ldap/groups.py
@@ -28,33 +28,17 @@ class LDAPGroupsConfig(object):
         self.queryFilter = queryFilter
 
 class LDAPGroup(LDAPPrincipal):
-    """An LDAP group, it checks compatobility with LDAP
-    """
-  def __init__ (self, principalSet = None):
-        zope.component.provideHandler(self._handler)
-	if userset:
-	  self._pSet = set(principalSet)
-	else:
-	  self._pSet = set()
-	
-  def _handler(self,event):
-	""" events regarding principals are passed here as soon as they happen
-	"""
-    
-  def add(self,LDAPPrincipal):
-    """Add the user/group to a set and subscribe to signals the user sends
-    """
-  
-  def remove_user(self, LDAPPrincipal):
-    """Removes a user/group from the group
-    """
-    
-  def update(self):
-    """Updates the group from the server
-    """
-    
-  def __call__():
-    """Commits changes made
+    """An LDAP group
+
+        for groups children are found by:
+        - we have a multivalue attribute pointing to member dns
+        - has an attribute pointing to our dn
+        - we have an attribute that matches another attribute on the user
+
+        AD: dn:memberOf
+        openldap: member:dn
+        posix: memberUid:uidNumber|gidNumber:gidNumber
+        arbitrary: group_attr:user_attr  |   &    ()
     """
 
 class LDAPGroups(LDAPPrincipals):
@@ -63,5 +47,3 @@ class LDAPGroups(LDAPPrincipals):
     def __init__(self, cfg):
         super(LDAPGroups, self).__init__(cfg)
         self._ChildClass = LDAPGroup
-    
-	
