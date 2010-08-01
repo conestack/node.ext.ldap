72661c26116e1473f8acf99c9f44bff8a5a82773
jperovic <jperovic@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-08-01 13:31:47 +0000

    WIP - LDAPRelationFilter + addidional tests
    git-svn-id: https://svn.plone.org/svn/collective/bda.ldap/trunk@122600 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- src/bda/ldap/filter.py
+++ src/bda/ldap/filter.py
@@ -3,7 +3,12 @@ from ldap.filter import filter_format
 class LDAPFilter(object):
     def __init__(self, queryFilter=None):
         # We expect queryFilter to be correctly escaped
+        if queryFilter is not None and not isinstance(queryFilter, basestring) and not isinstance(queryFilter, LDAPFilter):
+            raise TypeError('Query filter must be string')
+
         self._filter = queryFilter
+        if isinstance(queryFilter, LDAPFilter):
+            self._filter = queryFilter.__str__()
 
     def __and__(self, other):
         if other is None:
@@ -31,12 +36,8 @@ class LDAPFilter(object):
         elif not isinstance(other, basestring):
             raise TypeError(u"unsupported operand type")
         us = self.__str__()
-        if us or other:
+        if us and other:
             res = '(|%s%s)' % (us, other)
-        elif us:
-            res = us
-        elif other:
-            res = other
         return LDAPFilter(res)
 
     def __contains__(self, attr):
@@ -59,20 +60,59 @@ class LDAPDictFilter(LDAPFilter):
         return self.criteria and dict_to_filter(criteria=self.criteria, or_search=self.or_search).__str__() or ''
 
     def __repr__(self):
-        return "LDAPDictFilter(criteria=%r)" % (self.criteria,)
+        return "LDAPDictFilter(criteria=%r)" (self.criteria,)
 
+class FooNode(object):
+    def __init__(self, attrs):
+        self.attrs = attrs
+
+class LDAPRelationFilter(LDAPFilter):
+    def __init__(self, node, relation):
+	self.relation = relation
+        self.gattrs = node.attrs
+
+
+    def __str__(self):
+        """turn relation string into ldap filter string
+        """
+        _filter = LDAPFilter()
+        dictionary = dict()
+
+        for k,v in parsedRelation.items():
+            if v.__str__() == '' or k.__str__() == '':
+                continue
+            dictionary[v.__str__()] = self.gattrs[k.__str__()]
+
+        self.dictionary = dictionary
+
+        if len(dictionary) is 1:
+            _filter = LDAPFilter(self.relation)
+        else:
+            _filter = dict_to_filter(parsedRelation, True)
+
+	return self.dictionary and dict_to_filter(criteria=self.dictionary,
+                or_search=True ).__str__() or ''
 
 def dict_to_filter(criteria, or_search):
     """Turn dictionary criteria into ldap queryFilter string
     """
-    _filter = LDAPFilter()
+    _filter = None
     for attr, values in criteria.items():
         if not isinstance(values, list):
             values = [values]
 	if ( or_search ):
 		for value in values:
-			_filter |= '(%s=%s)' % (attr, value)
+		    if _filter is None:
+                        _filter = LDAPFilter( '(%s=%s)' % (attr, value))
+                    else:
+                        _filter |= '(%s=%s)' % (attr, value)
 	else:
 		for value in values:
+                    if _filter is None:
+                        _filter = LDAPFilter( '(%s=%s)' % (attr, value))
+                    else:
 			_filter &= '(%s=%s)' % (attr, value)
+        if _filter is None:
+            _filter = LDAPFilter()
+
     return _filter
