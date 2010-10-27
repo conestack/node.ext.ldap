2b7a3918baa817a23a72e87e53c35c650f1444eb
Florian Friesdorf <flo@chaoflow.net>
2010-10-27 22:11:12 +0200

    interfaces rework

--- src/bda/ldap/interfaces.py
+++ src/bda/ldap/interfaces.py
@@ -4,114 +4,123 @@
 from zope.interface import Interface
 from zope import schema
 
+from bda.ldap import SUBTREE
+
+
 class ICacheProviderFactory(Interface):
     """Create some ICacheProvider implementing object on __call__.
 
     Must be registered as utility.
     """
-
     def __call__():
         """See above.
         """
+
 
 class ILDAPProps(Interface):
     """LDAP properties configuration interface.
     """
     
     uri = schema.TextLine(
-        title=u"URI",
-        description=u"overrides server/port, forget about server and port, use"
-                    u"this to specify how to access the ldap server",
-        required=True)
+            title=u"URI",
+            description=u"e.g. ldap://localhost:389/",
+            required=True,
+            )
     
     user = schema.TextLine(
-        title=u"user",
-        description=u"",
-        required=True)
+            title=u"Bind DN",
+            description=u"DN used to bind to the ldap server.",
+            required=True,
+            )
     
     password = schema.TextLine(
-        title=u"password",
-        description=u"",
-        required=True)
+            title=u"Bind password",
+            description=u"Password used to bind to the ldap server.",
+            required=True,
+            )
     
-    cache = schema.TextLine(
-        title=u"cache",
-        description=u"",
-        required=True)
+    cache = schema.Bool(
+            title=u"Cache",
+            description=u"Enable caching",
+            required=True,
+            )
     
     timeout = schema.TextLine(
-        title=u"timeout",
-        description=u"",
-        required=True)
-    
-    start_tls = schema.TextLine(
-        title=u"start_tls",
-        description=u"",
-        required=True)
-    
-    tls_cacertfile = schema.TextLine(
-        title=u"tls_cacertfile",
-        description=u"",
-        required=True)
-    
-    tls_cacertdir = schema.TextLine(
-        title=u"tls_cacertdir",
-        description=u"",
-        required=True)
-    
-    tls_clcertfile = schema.TextLine(
-        title=u"tls_clcertfile",
-        description=u"",
-        required=True)
-    
-    tls_clkeyfile = schema.TextLine(
-        title=u"tls_clkeyfile",
-        description=u"",
-        required=True)
-    
+            title=u"Cache timeout",
+            description=u"Cache timeout in seconds",
+            default=600,
+            required=True,
+            )
+
+#    start_tls = schema.TextLine(
+#        title=u"start_tls",
+#        description=u"",
+#        required=True)
+#
+#    tls_cacertfile = schema.TextLine(
+#        title=u"tls_cacertfile",
+#        description=u"",
+#        required=True)
+#
+#    tls_cacertdir = schema.TextLine(
+#        title=u"tls_cacertdir",
+#        description=u"",
+#        required=True)
+#
+#    tls_clcertfile = schema.TextLine(
+#        title=u"tls_clcertfile",
+#        description=u"",
+#        required=True)
+#
+#    tls_clkeyfile = schema.TextLine(
+#        title=u"tls_clkeyfile",
+#        description=u"",
+#        required=True)
+#
     retry_max = schema.TextLine(
-        title=u"retry_max",
-        description=u"",
-        required=True)
+            title=u"Retry max",
+            description=u"Maximum number of reconnection attempts",
+            default=1,
+            required=True,
+            )
     
     retry_delay = schema.TextLine(
-        title=u"retry_delay",
-        description=u"",
-        required=True)
+            title=u"Retry delay",
+            description=u"Delay between reconnection attempts",
+            default=5.0,
+            required=True,
+            )
 
 
-#XXX: ILDAPPrincipalsConfig
-#XXX: make sure attrmap contains id for principals and login for users
-
-class ILDAPGroupsConfig(Interface):
-    """LDAP groups configuration interface.
+class ILDAPPrincipalsConfig(Interface):
     """
-    
-    props = schema.Object(
-        title=u"LDAPProps object",              
-        schema=ILDAPProps)
-    
+    """
     baseDN = schema.TextLine(
-        title=u"Base DN",
-        required=True)
-    
-    id_attr = schema.TextLine(
-        title=u"ID attribute name",
-        required=True)
-    
-    #scope = schema.Choice(
-    #    title="Scope for searching",
-    #    vocabulary=None) # XXX
-    scope = schema.TextLine(
-        title=u"Scope for searching")
-    
-    queryFilter = schema.TextLine(
-        title=u"Query filter")
+            title=u"Base DN",
+            required=True,
+            )
 
-class ILDAPUsersConfig(ILDAPGroupsConfig):
+#    attrmap = schema.Dict(
+#            title=u"Attribute mapping",
+#            description=u"The minimum required is 'id' and 'login'."
+#            required=True,
+#            )
+#
+    scope = schema.Choice(
+            title="Search scope",
+            values=(ONELEVEL, SUBTREE),
+            )
+
+    queryFilter = schema.TextLine(
+            title=u"Query filter"
+            )
+
+
+class ILDAPUsersConfig(ILDAPPrincipalsConfig):
     """LDAP users configuration interface.
     """
-    
-    login_attr = schema.TextLine(
-        title=u"login attribute name",
-        required=True)
+
+
+class ILDAPGroupsConfig(ILDAPPrincipalsConfig):
+    """LDAP groups configuration interface.
+    """
