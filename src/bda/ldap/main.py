0e90f6a2cf3b80baf0409e399eb039838d50aec4
Robert Niederreiter <rnix@squarewave.at>
2010-11-10 14:48:25 +0100

    wip

--- src/bda/ldap/main.py
+++ src/bda/ldap/main.py
@@ -17,21 +17,23 @@ CMD = {
 }
 
 def slapd():
-    import os
-    print os.environ['SLAPD_BIN']
-    print os.environ['SLAPD_URIS']
-    print os.environ['LDAP_DELETE_BIN']
-    print os.environ['LDAP_ADD_BIN']
-    return
+    #import os
+    #print os.environ['SLAPD_BIN']
+    #print os.environ['SLAPD_URIS']
+    #print os.environ['LDAP_DELETE_BIN']
+    #print os.environ['LDAP_ADD_BIN']
+    #return
     args = sys.argv[1:]
     if len(args) != 2:
         print u"Usage: ./bin/testldap LDIFLayer start|stop"
         sys.exit(2)
-    layer = getattr(testing, args[0])
-    if not layer:
+    try:
+        layer = getattr(testing, args[0])
+    except AttributeError:
         print u"Given layer not found: %s" % args[0]
         sys.exit(2)
     cmd = args[1]
     if not cmd in CMD.keys():
         print u"Unknown command: %s" % cmd
+        sys.exit(2)
     CMD[cmd](layer)
