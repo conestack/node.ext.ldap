ffece9ed518f1746ee3e2f54736e4ed6ac54f2ab
Florian Friesdorf <flo@chaoflow.net>
2011-02-28 07:28:16 +0100

    indent

--- src/node/ext/ldap/__init__.py
+++ src/node/ext/ldap/__init__.py
@@ -2,21 +2,21 @@ from node.ext.ldap.scope import (
     BASE,
     ONELEVEL,
     SUBTREE,
-)
+    )
 from node.ext.ldap.base import (
     LDAPConnector,
     LDAPCommunicator,
-)
+    )
 from node.ext.ldap.strcodec import StrCodec
 from node.ext.ldap.properties import (
     LDAPServerProperties,
     LDAPProps,
-)
+    )
 from node.ext.ldap.session import LDAPSession
 from node.ext.ldap.bbb import (
     LDAPNode,
     queryNode,
-)
+    )
 from node.ext.ldap.schema import LDAPSchemaInfo
 # legacy
 import base
