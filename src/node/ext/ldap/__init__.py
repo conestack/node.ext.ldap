0cd667319a42c95abc209e790bdf3d5b8cb19077
Florian Friesdorf <flo@chaoflow.net>
2011-02-28 07:32:41 +0100

    reordered

--- src/node/ext/ldap/__init__.py
+++ src/node/ext/ldap/__init__.py
@@ -1,24 +1,27 @@
+from node.ext.ldap.base import (
+    LDAPCommunicator,
+    LDAPConnector,
+    )
+from node.ext.ldap.properties import (
+    LDAPProps,
+    LDAPServerProperties,
+    )
+from node.ext.ldap.schema import LDAPSchemaInfo
 from node.ext.ldap.scope import (
     BASE,
     ONELEVEL,
     SUBTREE,
     )
-from node.ext.ldap.base import (
-    LDAPConnector,
-    LDAPCommunicator,
-    )
-from node.ext.ldap.strcodec import StrCodec
-from node.ext.ldap.properties import (
-    LDAPServerProperties,
-    LDAPProps,
-    )
 from node.ext.ldap.session import LDAPSession
+from node.ext.ldap.strcodec import StrCodec
+
+# to be removed sooner or later
 from node.ext.ldap.bbb import (
     LDAPNode,
     queryNode,
     )
-from node.ext.ldap.schema import LDAPSchemaInfo
-# legacy
+
+# legacy - can we get rid of this?
 import base
 import properties
 import session
