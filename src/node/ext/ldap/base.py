fe36b8d7a8e5b7978d96a2f69d3ed055cf2e8051
Florian Friesdorf <flo@chaoflow.net>
2011-03-02 18:08:52 +0100

    whitespace cleanup

--- src/node/ext/ldap/base.py
+++ src/node/ext/ldap/base.py
@@ -32,9 +32,9 @@ def testLDAPConnectivity(server=None, port=None, props=None):
 
 def md5digest(key):
     """Needed to support both, python 2.4 and python >=2.5
-    
+
     Will be remove when python 2.4 support is dropped.
-    
+
     XXX: This is maybe expensive. Optimize code eventually.
     """
     try:
@@ -50,20 +50,20 @@ def md5digest(key):
 
 class LDAPConnector(object):
     """Object is responsible for the LDAP connection.
-    
+
     This Object knows about the LDAP Server to connect to, the authentication
     information and the protocol to use.
-    
+
     TODO: TLS/SSL Support.
-    
+
     Normally you do not need to use this class directly.
-        
+
     >>> c = LDAPConnector('localhost', 389, 'cn=admin,dc=foo,dc=bar', 'secret')
     >>> connection = c.bind()
     >>> # do something with connection
-    >>> c.unbind() 
+    >>> c.unbind()
     """
-    
+
     def __init__(self,
                  server=None,
                  port=None,
@@ -73,7 +73,7 @@ class LDAPConnector(object):
                  cachetimeout=43200,
                  props=None):
         """Initialize LDAPConnector.
-        
+
         Signature Deprecated: Signature will take ``LDAPServerProperties``
                               object instead of current kwargs in future.
                               This will be changed in Version 1.5.
@@ -93,15 +93,15 @@ class LDAPConnector(object):
             self._bindPW = props.password
             self._cache = props.cache
             self._cachetimeout = props.timeout
-    
+
     def setProtocol(self, protocol):
         """Set the LDAP Protocol Version to use.
-        
+
         Deprecated: This function will be removed in version 1.5. Use
                     ``protocol`` property directly instead.
         """
         self.protocol = protocol
-    
+
     def bind(self):
         """Bind to Server and return the Connection Object.
         """
@@ -109,7 +109,7 @@ class LDAPConnector(object):
         self._con.protocol_version = self.protocol
         self._con.simple_bind(self._bindDN, self._bindPW)
         return self._con
-    
+
     def unbind(self):
         """Unbind from Server.
         """
@@ -120,10 +120,10 @@ class LDAPConnector(object):
 class LDAPCommunicator(object):
     """Class LDAPCommunicator is responsible for the communication with the
     LDAP Server.
-    
+
     It provides methods to search, add, modify and delete entries in the
     directory.
-    
+
     Usage:
 
     c = LDAPConnector('localhost', 389, 'cn=admin,dc=foo,dc=bar', 'secret')
@@ -135,7 +135,7 @@ class LDAPCommunicator(object):
     ...
     lc.unbind()
     """
-    
+
     def __init__(self, connector):
         """Takes LDAPConnector object as argument.
         """
@@ -147,44 +147,44 @@ class LDAPCommunicator(object):
             cachefactory = queryUtility(ICacheProviderFactory)
             if cachefactory is None:
                 cachefactory = nullcacheProviderFactory
-            cacheprovider = cachefactory()          
+            cacheprovider = cachefactory()
             self._cache = ICacheManager(cacheprovider)
             self._cache.setTimeout(connector._cachetimeout)
             logger.debug(u"LDAP Caching activated for instance '%s'. Use '%s' "
                           "as cache provider" % (repr(self._cache),
                                                  repr(cacheprovider)))
-        
+
     def bind(self):
         """Bind to LDAP Server.
         """
         self._con = self._connector.bind()
-        
+
     def unbind(self):
         """Unbind from LDAP Server.
         """
         self._connector.unbind()
         self._con = None
-        
+
     def setBaseDN(self, baseDN):
         """Set the base DN you want to work on.
-        
+
         Deprecated: This function will be removed in version 1.5. Use
                     ``baseDN`` property directly instead.
         """
         self.baseDN = baseDN
-        
+
     def getBaseDN(self):
         """Returns the current set base DN.
-        
+
         Deprecated: This function will be removed in version 1.5. Use
                     ``baseDN`` property directly instead.
         """
         return self.baseDN
-        
+
     def search(self, queryFilter, scope, baseDN=None,
                force_reload=False, attrlist=None, attrsonly=0):
         """Search the directory.
-        
+
         ``queryFilter``
             LDAP query filter
         ``scope``
@@ -212,13 +212,13 @@ class LDAPCommunicator(object):
                                        force_reload, args)
         return self._con.search_s(baseDN, scope, queryFilter,
                                   attrlist, attrsonly)
-    
+
     def add(self, dn, data):
         """Insert an entry into directory.
-        
+
         Takes the DN of the entry and the data this object contains. data is a
         dict and looks like this:
-        
+
         >>> data = {
         ...     'uid':'foo',
         ...     'givenname':'foo',
@@ -233,22 +233,22 @@ class LDAPCommunicator(object):
         ... }
         """
         attributes = [ (k,v) for k,v in data.items() ]
-        self._con.add_s(dn, attributes)    
-        
+        self._con.add_s(dn, attributes)
+
     def modify(self, dn, modlist):
         """Modify an existing entry in the directory.
-        
-        Takes the DN of the entry and the modlist, which is a list of tuples 
-        containing modifation descriptions. The first element gives the type 
-        of the modification (MOD_REPLACE, MOD_DELETE, or MOD_ADD), the second 
-        gives the name of the field to modify, and the third gives the new 
+
+        Takes the DN of the entry and the modlist, which is a list of tuples
+        containing modifation descriptions. The first element gives the type
+        of the modification (MOD_REPLACE, MOD_DELETE, or MOD_ADD), the second
+        gives the name of the field to modify, and the third gives the new
         value for the field (for MOD_ADD and MOD_REPLACE).
         """
         self._con.modify_s(dn, modlist)
-        
+
     def delete(self, deleteDN):
         """Delete an entry from the directory.
-        
+
         Take the DN to delete from the directory as argument.
         """
         self._con.delete_s(deleteDN)
@@ -256,10 +256,10 @@ class LDAPCommunicator(object):
     def passwd(self, userdn, oldpw, newpw):
         self._con.passwd_s(userdn, oldpw, newpw)
 
-def main():    
+def main():
     """Use this module from command line for testing the connectivity to the
     LDAP Server.
-    
+
     Expects server and port as arguments.
     """
     import sys
@@ -268,6 +268,6 @@ def main():
     else:
         server, port = sys.argv[1:]
         print testLDAPConnectivity(server, int(port))
-        
+
 if __name__ == "__main__":
     main()
--- src/node/ext/ldap/session.py
+++ src/node/ext/ldap/session.py
@@ -11,12 +11,12 @@ from node.ext.ldap.base import testLDAPConnectivity
 from node.ext.ldap.strcodec import encode, decode
 
 class LDAPSession(object):
-    
+
     def __init__(self, props):
         self._props = props
         connector = LDAPConnector(props=props)
         self._communicator = LDAPCommunicator(connector)
-    
+
     def checkServerProperties(self):
         """Test if connection can be established.
         """
@@ -25,29 +25,29 @@ class LDAPSession(object):
             return (True, 'OK')
         else:
             return (False, res)
-    
+
     def setBaseDN(self, baseDN):
         """Set the base DN you want to work on.
-        
+
         Deprecated: This function will be removed in version 1.5. Use
                     ``baseDN`` property directly instead.
         """
         self.baseDN = baseDN
-    
+
     def _get_baseDN(self):
         baseDN = self._communicator.baseDN
         baseDN = decode(baseDN)
         return baseDN
-    
+
     def _set_baseDN(self, baseDN):
         if isinstance(baseDN, str):
             # make sure its utf8
             baseDN = decode(baseDN)
         baseDN = encode(baseDN)
         self._communicator.baseDN = baseDN
-    
+
     baseDN = property(_get_baseDN, _set_baseDN)
-    
+
     def search(self, queryFilter='(objectClass=*)', scope=BASE, baseDN=None,
                force_reload=False, attrlist=None, attrsonly=0):
         # It makes no sense to really pass these to LDAP, therefore, we
@@ -61,11 +61,11 @@ class LDAPSession(object):
         # ActiveDirectory returns entries with dn None, which can be ignored
         res = filter(lambda x: x[0] is not None, res)
         return res
-    
+
     def add(self, dn, data):
         func = self._communicator.add
         return self._perform(func, dn, data)
-    
+
     def authenticate(self, dn, pw):
         """Verify credentials, but don't rebind the session to that user
         """
@@ -80,35 +80,35 @@ class LDAPSession(object):
 
     def modify(self, dn, data, replace=False):
         """Modify an existing entry in the directory.
-        
+
         @param dn: Modification DN
         @param data: either list of 3 tuples (look at
                      node.ext.ldap.base.LDAPCommunicator.modify for details), or
                      a dictionary representing the entry or parts of the entry.
         @param replace: if set to True, replace entry at DN entirely with data.
-        
+
         XXX: implement described behaviour for data
         """
         func = self._communicator.modify
         return self._perform(func, dn, data)
-    
+
     def delete(self, dn):
         func = self._communicator.delete
         return self._perform(func, dn)
-    
+
     def passwd(self, userdn, oldpw, newpw):
         func = self._communicator.passwd
         self._perform(func, userdn, oldpw, newpw)
 
     def unbind(self):
         self._communicator.unbind()
-    
+
     def _perform(self, function, *args, **kwargs):
         """Try to perform the given function with the given argument.
-        
+
         If LDAP directory is down, bind again and retry given function.
-        
-        XXX: * Improve retry logic in LDAPSession 
+
+        XXX: * Improve retry logic in LDAPSession
              * Extend LDAPSession object to handle Fallback server(s)
         """
         args = encode(args)
