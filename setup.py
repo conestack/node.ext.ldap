0cdd6a69394c75b8877c0025f8177bdca89ce6d4
Florian Friesdorf <flo@chaoflow.net>
2011-03-21 19:12:19 +0100

    0.9b19

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.9b10'
+version = '0.9b19'
 shortdesc = "Extending the nodespace towards LDAP"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
