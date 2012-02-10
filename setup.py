458301a7383c68c3cb7302fd45eae776c779dabb
Robert Niederreiter <rnix@squarewave.at>
2012-02-10 15:44:14 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.9.1htug1'
+version = '0.9.1htug2'
 shortdesc = "Node based LDAP support"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()
