e9c877bc62e13d0928d97d7429150936b5a4637e
Robert Niederreiter <office@squarewave.at>
2012-08-16 17:51:18 +0200

    "bda.recipe.deployment pre tag commit"

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.9.4dev'
+version = '0.9.4htug1'
 shortdesc = "Node based LDAP support"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()
