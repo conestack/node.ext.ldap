f8979222a13e9844b3772b3fe0b26403a673cb56
Robert Niederreiter <office@squarewave.at>
2017-12-07 08:29:57 +0100

    No complaints if directories not exists in bootstrap, ignores

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
-rm -r ./lib ./include ./local ./bin
+for dir in lib include local bin; do
+    if [ -d "$dir" ]; then
+        rm -r "$dir"
+    fi
+done
 virtualenv --clear .
 ./bin/pip install --upgrade pip setuptools zc.buildout
 ./bin/buildout
