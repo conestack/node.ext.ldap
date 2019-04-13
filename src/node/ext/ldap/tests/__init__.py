87945bae105699948e3d8316d4224dc4f9291dd0
Robert Niederreiter <office@squarewave.at>
2019-04-13 19:31:10 +0200

    Convert ugm defaults doctests to unittests done

--- src/node/ext/ldap/tests/__init__.py
+++ src/node/ext/ldap/tests/__init__.py
@@ -3,11 +3,23 @@ import unittest
 
 
 def test_suite():
+    from node.ext.ldap.tests import test_base
+    from node.ext.ldap.tests import test_cache
+    from node.ext.ldap.tests import test_filter
     from node.ext.ldap.tests import test_node
+    from node.ext.ldap.tests import test_properties
+    from node.ext.ldap.tests import test_schema
+    from node.ext.ldap.tests import test_session
 
     suite = unittest.TestSuite()
 
+    suite.addTest(unittest.findTestCases(test_base))
+    suite.addTest(unittest.findTestCases(test_cache))
+    suite.addTest(unittest.findTestCases(test_filter))
     suite.addTest(unittest.findTestCases(test_node))
+    suite.addTest(unittest.findTestCases(test_properties))
+    suite.addTest(unittest.findTestCases(test_schema))
+    suite.addTest(unittest.findTestCases(test_session))
 
     return suite
 
