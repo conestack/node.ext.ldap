53c49ee37c80b1b030ed8b6c053f855c879fc13d
Robert Niederreiter <office@squarewave.at>
2019-04-14 17:08:47 +0200

    cleanup bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 set -e
+
+function cleanup() {
+    if [ -e "$1" ]; then
+        rm -rf $1
+    fi
+}
+
+function create_venv() {
+    virtualenv --clear --no-site-packages -p $1 $2
+}
+
+function copy_config() {
+    cp ../ldap.cfg .
+    cp ../versions.cfg .
+    cp ../base.cfg .
+    cp ../buildout.cfg .
+}
+
+function run_buildout() {
+    pushd $1
+    copy_config
+    ./bin/pip install --upgrade pip setuptools zc.buildout
+    ./bin/buildout
+    popd
+}
+
 if [ -x "$(which python)" ]; then
-    rm -r py2
-    virtualenv --clear --no-site-packages -p python py2
-    cd py2
-    cp ../ldap.cfg .
-    cp ../versions.cfg .
-    cp ../base.cfg .
-    cp ../buildout.cfg .
-    ./bin/pip install --upgrade pip setuptools zc.buildout
-    ./bin/buildout
-    cd ..
+    cleanup py2
+    create_venv python py2
+    run_buildout py2
 fi
+
 if [ -x "$(which python3)" ]; then
-    rm -r py3
-    virtualenv --clear --no-site-packages -p python3 py3
-    cd py3
-    cp ../ldap.cfg .
-    cp ../versions.cfg .
-    cp ../base.cfg .
-    cp ../buildout.cfg .
-    ./bin/pip install --upgrade pip setuptools zc.buildout
-    ./bin/buildout
-    cd ..
+    cleanup py3
+    create_venv python3 py3
+    run_buildout py3
 fi
