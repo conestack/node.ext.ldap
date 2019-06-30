16e234c473e2f8c21a3d5fe2f3ea20a7e1d16bc7
Robert Niederreiter <office@squarewave.at>
2019-06-30 13:49:59 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read_file(name):
         return f.read()
 
 
-version = '1.0b10'
+version = '1.0b11.dev0'
 shortdesc = 'LDAP/AD convenience with Node-trees based on python-ldap'
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.rst',
